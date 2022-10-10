edc9e6d1c18033474f8213ff02d3dbdeaa7a8c11
Minyewoo <const.trushov@gmail.com>
2022-10-10 20:26:22 +0500

    expanded pdf tests

--- text_extractor/tests/test_pdf_text_extractor.py
+++ text_extractor/tests/test_pdf_text_extractor.py
@@ -1,9 +1,9 @@
 import unittest
 from os import path
 import tempfile
+from reportlab.pdfgen import canvas
 from extractors.pdf_text_extractor import PdfTextExtractor
 from extraction_objects.pdf_extraction_object import PdfExtractionObject
-from reportlab.pdfgen import canvas
 from readers.pdf_reader import PdfReader
 
 
@@ -11,6 +11,9 @@ class TestPdfTextExtractor(unittest.TestCase):
     def setUp(self) -> None:
         self.extractor = PdfTextExtractor()
         self.test_filepath = path.join(tempfile.gettempdir(), 'test.pdf')
+
+        tests_dir_path = path.abspath(path.dirname(__file__))
+        self.samples_dir = path.join(tests_dir_path, 'test_examples')
 
     def _generate_pdf(self, text: str, filename: str) -> PdfExtractionObject:
         script_path = path.abspath(path.dirname(__file__))
@@ -26,6 +29,9 @@ class TestPdfTextExtractor(unittest.TestCase):
         c.save()
 
         return PdfReader().read(filename)
+    
+    def _read_file(self, path: str) -> PdfExtractionObject:
+        return PdfReader().read(path)
 
     def test_image_removal(self):
         text = ''
@@ -73,4 +79,36 @@ class TestPdfTextExtractor(unittest.TestCase):
         self.assertEqual(
             self.extractor.extract(eo),
             'Test\nTest'
+        )
+
+    def test_no_alphabetic_chars(self):
+        filepath = path.join(self.samples_dir, 'symbols.pdf')
+        eo = self._read_file(filepath)
+        self.assertEqual(
+            self.extractor.extract(eo),
+            '!"#$%&\'()*+,-./0123456789:;<=>?@[\]^_`{|}‘’‚‛“”„'
+        )
+
+    def test_latin_alphabet(self):
+        filepath = path.join(self.samples_dir, 'latin.pdf')
+        eo = self._read_file(filepath)
+        self.assertEqual(
+            self.extractor.extract(eo),
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZ\nabcdefghijklmnopqrstuvwxyz'
+        )
+
+    def test_cyrillic_alphabet(self):
+        filepath = path.join(self.samples_dir, 'cyrillic.pdf')
+        eo = self._read_file(filepath)
+        self.assertEqual(
+            self.extractor.extract(eo),
+            'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ\nабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+        )
+
+    def test_empty_text(self):
+        text = ''
+        obj = self._generate_pdf(text, self.test_filepath)
+        self.assertEqual(
+            self.extractor.extract(obj),
+            text
         )
