0dabdabc166e5d0ef772f685a8de8a162f2c9f31
Minyewoo <const.trushov@gmail.com>
2022-11-07 20:54:43 +0500

    added parser tests

--- module_2/src/post.py
+++ module_2/src/post.py
@@ -20,3 +20,19 @@ class Post:
         self.likes = likes
         self.reposts = reposts
         self.views = views
+
+    def __eq__(self, other):
+        if not isinstance(other, Post):
+            return False
+
+        return (
+            self.post_id == other.post_id and
+            self.group_id == other.group_id and
+            self.publisher_id == other.publisher_id and
+            self.date == other.date and
+            self.text == other.text and
+            self.comments == other.comments and
+            self.likes == other.likes and
+            self.reposts == other.reposts and
+            self.views == other.views
+        )
--- module_2/tests/test_parser.py
+++ module_2/tests/test_parser.py
@@ -1,11 +1,90 @@
 import unittest
-
+from src.post import Post
 from src.parser import Parser
 
 class TestParser(unittest.TestCase):
     def setUp(self) -> None:
-        self.parser = Parser([])
+        self.parser = Parser(['test'])
+
+        self.payload = {
+            'id': 0,
+            'owner_id': 0,
+            'from_id': 0,
+            'date': 0,
+            'text': 'test text',
+            'comments': {
+                'count': 0,
+            },
+            'likes': {
+                'count': 0,
+            },
+            'reposts': {
+                'count': 0,
+            },
+            'views': {
+                'count': 0,
+            },
+        }
+
+        self.post = Post(
+            post_id=self.payload['id'],
+            group_id=self.payload['owner_id'],
+            publisher_id=self.payload['from_id'],
+            date=self.payload['date'],
+            text=self.payload['text'],
+            comments=self.payload['comments']['count'],
+            likes=self.payload['likes']['count'],
+            reposts=self.payload['reposts']['count'],
+            views=self.payload['views']['count'],
+        )
         return super().setUp()
 
-    def test_parse_should_pass(self):
-        self.assertEqual(self.parser.parse(), None)
+    def test_parser_parse_with_filter_words(self):
+        post = self.parser.parse(self.payload)
+        self.assertEqual(post, self.post)
+
+    def test_parser_parse_without_filter_words(self):
+        parser = Parser([])
+        post = parser.parse(self.payload)
+        self.assertEqual(post, None)
+
+    def test_parser_parse_without_views(self):
+        del self.payload['views']
+        post = self.parser.parse(self.payload)
+        self.assertEqual(post.views, 'NULL')
+
+    def test_parser_trimming_text(self):
+        self.payload['text'] = '        test text           \n'
+        post = self.parser.parse(self.payload)
+        self.assertEqual(post.text, 'test text')
+
+    def test_parser_e_replacing(self):
+        self.payload['text'] = 'test tёxt ёж'
+        post = self.parser.parse(self.payload)
+        self.assertEqual(post.text, 'test tеxt еж')
+
+    def test_parser_lowercase_transition(self):
+        self.payload['text'] = 'TEST TEXT'
+        post = self.parser.parse(self.payload)
+        self.assertEqual(post.text, 'test text')
+
+    def test_parser_space_symbols_removal(self):
+        self.payload['text'] = '\n\rtest\ntext\t\n'
+        post = self.parser.parse(self.payload)
+        self.assertEqual(post.text, 'test text')
+
+    def test_parser_multiple_spaces_replacing(self):
+        self.payload['text'] = 'test            text'
+        post = self.parser.parse(self.payload)
+        self.assertEqual(post.text, 'test text')
+
+    def test_parser_links_removal(self):
+        self.payload['text'] = 'test https://vk.com https://spbu.ru text'
+        post = self.parser.parse(self.payload)
+        self.assertEqual(post.text, 'test text')
+
+    def test_random_characters(self):
+        s = '$_?@..$(,:[#}{*?.{%?/@?&!@*&,#'
+        self.payload['text'] = f'test text {s}'
+        post = self.parser.parse(self.payload)
+        self.assertEqual(post.text, 'test text ? .. (,: *?. %?/ ? ! * ,')
