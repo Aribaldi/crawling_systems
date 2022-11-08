fb8d88f5ea7bc8c69d1ef6a19c284fc08da08023
Minyewoo <const.trushov@gmail.com>
2022-11-08 05:32:12 +0500

    added downloader tests

--- module_2/tests/test_downloader.py
+++ module_2/tests/test_downloader.py
@@ -1,19 +1,81 @@
 from datetime import datetime
 import unittest
-import vk_api
-import yaml
+from unittest.mock import MagicMock, Mock
+from src.queue import Queue
 from src.downloader import Downloader
+
+class MockVK:
+    def __init__(self) -> None:
+        self.wall = self.MockWall()
+        self.newsfeed = self.MockNewsfeed()
+        self.groups = self.MockGroups()
+
+    class MockWall:
+        def get(self):
+            pass
+
+    class MockNewsfeed:
+        def search(self):
+            pass
+
+    class MockGroups:
+        def search(self):
+            pass
 
 class TestDownloader(unittest.TestCase):
     def setUp(self) -> None:
-        with open("config.yaml", "r", encoding="utf-8") as f:
-            cfg = yaml.load(f, Loader=yaml.FullLoader)
-
-        vk_session = vk_api.VkApi(**cfg["access_params"])
-        vk_session.auth()
-        vk = vk_session.get_api()
-        self.downloader = Downloader(vk, datetime.now())
+        self.vk = MockVK()
+        self.start_datetime = datetime.now()
+        self.vk.newsfeed.search = Mock(return_value={'items':[]})
+        self.vk.groups.search = Mock(return_value={'items':[]})
+        self.vk.wall.get = Mock(return_value={'items':[]})
+        self.downloader = Downloader(self.vk, datetime(2022, 11, 4).isoformat())
         return super().setUp()
 
-    def test_should_download(self):
-        pass
+    def test_get_posts_by_query(self):
+        queue = Queue()
+        queue.push = MagicMock()
+        self.vk.newsfeed.search = Mock(
+            return_value={
+                'items': [
+                    {
+                        'id': 0,
+                        'owner_id': 0,
+                        'from_id': 0,
+                        'date': datetime(2022, 11, 3).isoformat(),
+                        'text': 'test text',
+                        'comments': {
+                            'count': 0,
+                        },
+                        'likes': {
+                            'count': 0,
+                        },
+                        'reposts': {
+                            'count': 0,
+                        },
+                        'views': {
+                            'count': 0,
+                        },
+                    },
+                ]
+            }
+        )
+        self.downloader.get_posts_by_query(query='test', queue_to_push=queue)
+
+        queue.push.assert_not_called()
+
+    def test_get_groups_by_query(self):
+        queue = Queue()
+        queue.push = MagicMock()
+
+        self.downloader.get_groups_by_query(query='test', queue_to_push=queue)
+
+        queue.push.assert_not_called()
+
+    def test_get_posts_from_group(self):
+        queue = Queue()
+        queue.push = MagicMock()
+
+        self.downloader.get_posts_from_group(group=(0, 'group'), queue_to_push=queue)
+
+        queue.push.assert_not_called()
